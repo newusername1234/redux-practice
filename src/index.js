eb17c8cbbd7f1cf5dcd57c0eb01dde079740acbe
Wes <wesleyayer92@gmail.com>
2020-02-11 09:38:25 -0500

    adds payload syntax

--- src/index.js
+++ src/index.js
@@ -1,78 +1,36 @@
-// Creating reducers
-// Write reducer functions that expect to receive the state objects and actions you created in the previous exercise.
-//  Make sure to use your action constants.
-
-// Movies
-// Favorite song
-// Burritos
-// Coffee
-// Lunch
-
-
-// {
-//     numberOfMovies: 1
-// }
-    
-// {
-//     favSong: 'Song Name'
-// }
-        
-// {
-//     burritosEaten: 9000
-// }
-    
-// {
-//     coffeesDrank: 3
-// }
-        
-// {
-//     kindOfSandwich: 'italian'
-// }
-
-// function actionAddMovie() {
-//     return {
-//         type: ADD_MOVIE
-//     }
-// }
 import { createStore } from 'redux';
 
 function actionChangeSong(favSong) {
     return {
         type: CHANGE_SONG,
-        favSong
+        payload: { favSong }
     }
 }
 
 function actionChangeSandwich(kindOfSandwich) {
     return {
         type: CHANGE_SANDWICH,
-        kindOfSandwich
+        payload: { kindOfSandwich }
     }
 }
 
-const addMovie = 'ADD_MOVIE';
 const CHANGE_SONG = 'CHANGE_SONG';
-const ADD_BURRITO = 'ADD_BURRITO';
-const ADD_COFFEE = 'ADD_COFFEE';
 const CHANGE_SANDWICH = 'CHANGE_SANDWICH';
 
 
-const sandwichState = {
-    kindOfSandwich: 'vito'
+const defaultState = {
+    kindOfSandwich: 'vito',
+    favSong: 'ocean avenue',
 }
 
-const songState = {
-    favSong: 'ocean avenue'
-}
-
-function changer(state, action) {
+function changer(state=defaultState, action) {
     const newState = { ...state };
     switch(action.type) {
         case CHANGE_SANDWICH:
-            newState.kindOfSandwich = action.kindOfSandwich;
+            newState.kindOfSandwich = action.payload.kindOfSandwich;
             break;
         case CHANGE_SONG:
-            newState.favSong = action.favSong;
+            newState.favSong = action.payload.favSong;
             break;
         default:
             break;
@@ -87,7 +45,7 @@ store.subscribe(() => {
     console.table(store.getState());
 });
 
-// store.dispatch(actionChangeSandwich('turkey tom'));
+store.dispatch(actionChangeSandwich('turkey tom'));
 store.dispatch(actionChangeSong('holiday'));     
 
 // import React from 'react';
